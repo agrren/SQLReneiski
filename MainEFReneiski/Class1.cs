8c3b128df2370710cba474c2a230d138d90cc419
Aliaksandr Reneiski <agrren@tut.by>
2024-04-25 16:01:56 +0300

    SQLHomeTask

---
+++ MainEFReneiski/Class1.cs
@@ -0,0 +1,34 @@
+﻿using EFReneiskiNew;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainEFReneiski
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+
+        }
+        
+
+        //private static void Grrr()
+        //{
+        //    using (var context = new AdonetdbEntities())
+        //    {
+        //        var name = context.Users.First(x => x.Id == 3).Name;
+        //        //ClassicAssert.AreEqual("Tom", name);
+
+        //        Console.WriteLine(name);
+
+        //        //var newUser = new User() { Age = 48, Name = "Gearge" };
+        //        //context.Users.Add(newUser);
+        //        //context.SaveChanges();
+        //    }
+        //}
+    }
+}
---
+++ MainEFnew/UserSqlHelper.cs
@@ -0,0 +1,52 @@
+﻿using EFReneiskiNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MainEFnew
+{
+    public class UserSqlHelper
+    {
+        public static List<T> GetAdonetDbInstance<T>(Func<AdonetdbEntities, IQueryable<T>> func)
+        {
+            using (var context = new AdonetdbEntities())
+            {
+                var result = func(context);
+
+                return result.ToList();
+            }
+        }
+
+        public static User GetUserNameById(int value) =>
+            GetAdonetDbInstance(context =>
+                from users in context.Users
+                where users.Id == value
+                select users).First();
+
+        public static User GetUserIdByName(string value) =>
+            GetAdonetDbInstance(context =>
+                from users in context.Users
+                where users.Name == value
+                select users).First();
+
+        public static void AddUserToDb(string nameValue, int ageValue)
+        {
+            using (var context = new AdonetdbEntities())
+            {
+                var newUser = new User() { Age = ageValue, Name = nameValue };
+                context.Users.Add(newUser);
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteUserFromDb(int id)
+        {
+            using (var context = new AdonetdbEntities())
+            {
+                var userToDelete = context.Users.FirstOrDefault(x => x.Id == id);
+                context.Users.Remove(userToDelete);
+                context.SaveChanges();
+            }
+        }
+    }
+}
